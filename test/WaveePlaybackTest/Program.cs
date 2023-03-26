497ad7a388a9d02f653ecccf3799d147f8f00cdb
Christos Karapasias <christos@marteco.nl>
2023-03-26 21:56:51 +0900

    update

--- test/WaveePlaybackTest/Program.cs
+++ test/WaveePlaybackTest/Program.cs
@@ -1,18 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
 using Microsoft.Extensions.Logging;
+using NAudio.Wave;
 using Serilog;
 using Serilog.Events;
 using Wavee.Playback;
 using Wavee.Playback.Converters;
+using Wavee.Playback.Item;
+using Wavee.Playback.Normalisation;
 using Wavee.Playback.Packets;
 using Wavee.Playback.Volume;
 using Wavee.Sinks.NAudio;
+using Wavee.UI.Models.Local;
 using Wavee.Vorbis;
 using Wavee.Vorbis.Decoder;
 using Wavee.Vorbis.IO;
+using ILogger = Serilog.ILogger;
 
-var logger = new LoggerConfiguration()
+ILogger logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug)
     .CreateLogger();
@@ -23,7 +29,7 @@ var loggerFactory = new LoggerFactory()
 var config = new WaveePlayerConfig();
 var converter = new StdAudioConverter();
 var volume = new SoftVolume(1.0);
-var loader = new FileLoader();
+var loader = new VorbisLoader();
 
 var player = new WaveePlayer(config,
     loader,
@@ -32,12 +38,7 @@ var player = new WaveePlayer(config,
 
 var path = "C:\\Users\\ckara\\Downloads\\test.ogg";
 
-var fss = new FSSource(path);
-var mss = new MediaSourceStream(fss, new MediaSourceStreamOptions(4096));
-var format = new OggReader(mss, FormatOptions.Default, loggerFactory);
-var track = format.DefaultTrack;
-var vorbisDecoder = new VorbisDecoder(track.StreamCodecParams);
-
+player.PlayTrack(path, true, 0);
 //player.PlayTrack(path, true, 0);
 var mn = new ManualResetEvent(false);
 mn.WaitOne();
@@ -76,4 +77,116 @@ public class FSSource : MediaSource
         get => _fileStream.Position;
         set => _fileStream.Position = value;
     }
+}
+
+public class VorbisLoader : ITrackLoader
+{
+    public Task<PlayerLoadedTrackData?> LoadTrackAsync(string trackId, double positionMs)
+    {
+        //check the format
+        using var file = TagLib.File.Create(trackId);
+        var bitrate =
+            file.Properties.AudioBitrate;
+
+        var audioItem = new LocalTrack
+        {
+            Title = file.Tag.Title ?? file.Name,
+        };
+        ILogger logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug)
+            .CreateLogger();
+
+        var loggerFactory = new LoggerFactory()
+            .AddSerilog(logger);
+        var playbackitem = new PlaybackItem
+        {
+            Item = audioItem
+        };
+
+        file.Dispose();
+        var bytesPerSecond = bitrate / 8;
+
+        //assume no normalisation data for now
+
+        //       file.Properties.AudioSampleRate,
+        //file.Properties.AudioChannels,
+        var normalisationData = NormalisationData.Default;
+
+        var fss = new FSSource(trackId);
+        var mss = new MediaSourceStream(fss, new MediaSourceStreamOptions(4096));
+        var format = new OggReader(mss, FormatOptions.Default, loggerFactory);
+        var track = format.DefaultTrack;
+        var vorbisDecoder = new VorbisDecoder(track.StreamCodecParams);
+
+        var decoder = new VorbisTestDecoder(format, vorbisDecoder, fss.Length);
+
+        var duration = TimeSpan.FromSeconds(decoder.TotalTime.TotalSeconds);
+
+        // Don't try to seek past the track's duration.
+        // If the position is invalid just start from
+        // the beginning of the track.
+        var position = TimeSpan.FromMilliseconds(positionMs);
+        if (position > duration)
+        {
+            position = TimeSpan.Zero;
+        }
+
+        // Ensure the starting position. Even when we want to play from the beginning,
+        // the cursor may have been moved by parsing normalisation data. This may not
+        // matter for playback (but won't hurt either), but may be useful for the
+        // passthrough decoder.
+        decoder.CurrentTime = position;
+        if (decoder.CurrentTime != position)
+        {
+            throw new InvalidOperationException("Failed to seek to the starting position");
+        }
+
+        // Ensure streaming mode now that we are ready to play from the requested position.
+        var streamLoaderController
+            = new StreamLoaderController();
+
+        streamLoaderController.SetStreamMode();
+
+        var isExplicit = false;
+
+        Debug.WriteLine("Loaded track: " + trackId);
+
+        return Task.FromResult(new PlayerLoadedTrackData
+        {
+            Decoder = new NAudioDecoder(decoder),
+            NormalisationData = normalisationData,
+            StreamLoaderController = streamLoaderController,
+            AudioItem = playbackitem,
+            BytesPerSecond = bytesPerSecond,
+            DurationMs = duration.TotalMilliseconds,
+            IsExplicit = isExplicit,
+            StreamPositionMs = decoder.CurrentTime.TotalMilliseconds
+        })!;
+    }
+}
+
+public class VorbisTestDecoder : WaveStream
+{
+    private readonly OggReader _oggReader;
+    private readonly VorbisDecoder _vorbisDecoder;
+
+    public VorbisTestDecoder(OggReader oggReader, VorbisDecoder vorbisDecoder, long length)
+    {
+        _oggReader = oggReader;
+        _vorbisDecoder = vorbisDecoder;
+        WaveFormat = new WaveFormat(
+            (int)_oggReader.DefaultTrack.StreamCodecParams.SampleRate,
+            (int)_oggReader.DefaultTrack.StreamCodecParams.Channels);
+        Length = length;
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override WaveFormat WaveFormat { get; }
+    public override long Length { get; }
+    public override long Position { get; set; }
 }
