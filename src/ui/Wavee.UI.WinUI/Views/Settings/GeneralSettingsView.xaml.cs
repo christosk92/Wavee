b1e3626ba54386c4bda12df2fd048073c9276558
Christos Karapasias <christos@marteco.nl>
2023-05-28 16:56:01 +0900

    Cache settings page

--- src/ui/Wavee.UI.WinUI/Views/Settings/GeneralSettingsView.xaml.cs
+++ src/ui/Wavee.UI.WinUI/Views/Settings/GeneralSettingsView.xaml.cs
@@ -12,6 +12,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml.Documents;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +27,11 @@ namespace Wavee.UI.WinUI.Views.Settings
         public GeneralSettingsView()
         {
             this.InitializeComponent();
+        }
+
+        private void GoToCache(Hyperlink sender, HyperlinkClickEventArgs args)
+        {
+            SettingsView.Instance.NavigateToCache();
         }
     }
 }
